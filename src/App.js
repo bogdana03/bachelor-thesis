12db0f7b8e09f0d0cd36c6ef36cc3debca6799e9
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-24 22:01:31 +0300

    Updated the design for the application & upadate the application so that is full responsive

--- src/App.js
+++ src/App.js
@@ -1,58 +1,81 @@
-import './App.css'
-import {Navbar, Nav, Container} from 'react-bootstrap'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom"
-import { About } from './components/About'
-import { Contact } from './components/Contact'
-import { Home } from './components/Home'
-import { useAuth0 } from '@auth0/auth0-react'
-import { Footer } from './components/Footer'
-import { Cards } from './components/Cards'
-
-
+import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Home } from "./components/Home";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Footer } from "./components/Footer";
+import { Cards } from "./components/Cards";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
-  
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
   return (
     <BrowserRouter>
-    <div className="App">
-       <Navbar bg="dark" variant="dark">
-        <Container className='Container-nav'>
-          <Navbar.Brand className='dev'href="#home"><span>Dev</span>Tech</Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Nav.Link as={Link} to="/">SERVICES</Nav.Link>
-            <Nav.Link as={Link} to="/about">SPECIALISTS</Nav.Link>
-            <Nav.Link as={Link} to="/contact">ABOUT</Nav.Link>
-            
-           
-
-            {isAuthenticated && <Nav.Link as={Link} to="/cards">PROJECTS</Nav.Link> }
-
-            {isAuthenticated && <button class="btn btn-dark" onClick={() => logout({ returnTo: window.location.origin})}>Logout</button> }
-            {!isAuthenticated && <button class="btn btn-dark" onClick={() => loginWithRedirect()}>LOGIN</button>} 
-          </Nav>
-        </Container>
-      </Navbar>
-          
-      <div>
-        <Routes>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/cards" element={<Cards/>}/>
-        </Routes>
+          <div className="App">
+        <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+          <Container>
+            <Navbar.Brand className="dev" href="#home">
+              <span>DeV</span>ART
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link as={Link} to="/">
+                  SERVICES
+                </Nav.Link>
+                <Nav.Link as={Link} to="/about">
+                  SPECIALISTS
+                </Nav.Link>
+                <Nav.Link as={Link} to="/contact">
+                  ABOUT
+                </Nav.Link>
+                {isAuthenticated && (
+                  <Nav.Link as={Link} to="/cards">
+                    PROJECTS
+                  </Nav.Link>
+                )}
+                {isAuthenticated ? (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                  >
+                    Logout
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => loginWithRedirect()}
+                  >
+                    LOGIN
+                  </button>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className="main-container">
+          <Container fluid>
+            <Row>
+              <Col>
+                <Routes>
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/" element={<Home />} />
+                  <Route path="/cards" element={<Cards />} />
+                </Routes>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <Footer />
       </div>
-    </div>
-      <Footer />
     </BrowserRouter>
-    
-  
   );
 }
 
 export default App;
+
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,7 @@ export const Footer = () => {
         <div className="container text-center">
             <div className="row">
              <div className="col-md-5 ft-1">
-                <h4><span>Dev</span>Tech</h4>
-                <p>Lorem ipsum dolor.</p>
+                <h5><span>DeV</span>ART</h5>
                 <div className="footer-icons">
                   <i class="fa-brands fa-facebook"></i> 
                   <i class="fa-brands fa-instagram"></i> 
@@ -17,7 +16,7 @@ export const Footer = () => {
                 </div>
              </div>
              <div className="col-md-3 ft-2">
-                  <h5>Quiq Links</h5>
+                  <h6>Quiq Links</h6>
                   <ul>
                     <li className="footer-items">
                       <a className="" href="/">Portfolio</a>
@@ -28,23 +27,19 @@ export const Footer = () => {
                     <li className="footer-items">
                       <a className="" href="/">Contact Us</a>
                     </li>
-                    <li className="footer-items">
-                       <a className="" href="/">Blogs</a>
-                    </li>
+                   
                   </ul>
              </div>
              <div className="col-md-4 ft-3">
-              <h5>Contact Info</h5>
+              <h6>Contact Info</h6>
               <p><i class="fa-solid fa-phone"></i>+404 9093997</p>
-              <p><i class="fa-solid fa-envelope"></i>devtech@mail.com</p>
+              <p><i class="fa-solid fa-envelope"></i>devart@mail.com</p>
               <p><i class="fa-solid fa-paper-plane"></i>Manhatan, New York</p>
             </div> 
             </div>
         </div>
     </div>
-    <div className="Last-Footer">
-      <p>Design By BEB</p>
-    </div>
+   
       {/* */}
     </>
    ) 
